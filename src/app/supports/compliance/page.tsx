2a4c885855b5356808ac06b2a40bc136a2a53eed
Trufflepieces <trufflepieces@Trufflepiecess-MacBook-Air.local>
2025-09-26 15:48:50 +0800

    add metadata

--- src/app/supports/compliance/page.tsx
+++ src/app/supports/compliance/page.tsx
@@ -1,5 +1,22 @@
 import { Navbar } from "@/components/organisms/Navbar";
 import { Footer } from "@/components/organisms/Footer";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+    nocache: true,
+    googleBot: {
+      index: false,
+      follow: false,
+      noimageindex: true,
+      "max-video-preview": -1,
+      "max-image-preview": "none",
+      "max-snippet": -1,
+    },
+  },
+};
 
 export default function CompliancePage() {
   return (
